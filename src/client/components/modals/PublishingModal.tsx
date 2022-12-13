158cc2624651047db49c728e42a420b975da7c30
Nils Jacobsen <nils.jacobsen98@gmail.com>
2022-12-13 11:50:21 +0100

    fixed some visuals

--- src/client/components/modals/PublishingModal.tsx
+++ src/client/components/modals/PublishingModal.tsx
@@ -29,7 +29,7 @@ export default function PublishingMobileModal() {
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex min-h-full items-end justify-center text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -39,7 +39,7 @@ export default function PublishingMobileModal() {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-sm sm:p-6">
+              <Dialog.Panel className="relative transform overflow-hidden rounded-t-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-sm sm:p-6">
                 {publishingOptions.map((option) => (
                   <div
                     key={option.title}
