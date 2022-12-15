deadcd9f8c61d6a2d744fd42e7874e1d99b3f629
Felix Haeberle <kontakt@felixhaeberle.de>
2022-12-15 11:30:12 +0100

    update seeding

--- src/test/seed/data.ts
+++ src/test/seed/data.ts
@@ -3,46 +3,97 @@ import { SeedData } from "./index";
 export const testData: SeedData = {
   users: [
     {
-      email: "nils.jacobsen98@gmail.com"
+      email: "nils.jacobsen98@gmail.com",
+      image: "https://avatars.githubusercontent.com/u/58360188?v=4",
+      provider: "github",
+      type: "oauth",
+      token_type: "bearer",
+      scope: "read:user,user:email",
+      providerAccountId: "58360188",
+    },
+    {
+      email: "kontakt@felixhaeberle.de",
+      image: "https://avatars.githubusercontent.com/u/34959078?v=4",
+      provider: "github",
+      type: "oauth",
+      token_type: "bearer",
+      scope: "read:user,user:email",
+      providerAccountId: "34959078",
+    }
+  ],
+  sites: [
+    {
+      id: "934293746928734928374",
+      subdomain: "nilsjacobsen",
+      extensions: [
+        {
+          id: "2376491287369123",
+          storeExtension: {
+            id: "w9909823049852034",
+            name: "Repositories"
+          },
+          underlayingApis: [
+            {
+              id: "87934759374985",
+              refreshToken: "9348793475347534",
+              accessToken: "9e8r9w7r98e7rt98",
+              apiConnector: {
+                id: "39749345763945",
+                name: "Github",
+                markdown: "This is a description"
+              },
+              apiResponses: [
+                {
+                  id: "93845793845938",
+                  response: "{name: recentProjects, data: ['First', 'Second']}",
+                  apiConnectorRoute: {
+                    id: "3948579374593",
+                    name: "RecentProjects",
+                    url: "/projects/recent"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: "934293746928734928374",
+      subdomain: "felixhaeberle",
+      extensions: [
+        {
+          id: "2376491287369123",
+          storeExtension: {
+            id: "w9909823049852034",
+            name: "Repositories"
+          },
+          underlayingApis: [
+            {
+              id: "87934759374985",
+              refreshToken: "9348793475347534",
+              accessToken: "9e8r9w7r98e7rt98",
+              apiConnector: {
+                id: "39749345763945",
+                name: "Github",
+                markdown: "This is a description"
+              },
+              apiResponses: [
+                {
+                  id: "93845793845938",
+                  response: "{name: recentProjects, data: ['First', 'Second']}",
+                  apiConnectorRoute: {
+                    id: "3948579374593",
+                    name: "RecentProjects",
+                    url: "/projects/recent"
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ]
     },
   ],
 };
 
-// sites: [
-//   {
-//     id: "934293746928734928374",
-//     subdomain: "nilsjacobsen",
-//     extensions: [
-//       {
-//         id: "2376491287369123",
-//         storeExtension: {
-//           id: "w9909823049852034",
-//           name: "Repositories"
-//         },
-//         underlayingApis: [
-//           {
-//             id: "87934759374985",
-//             refreshToken: "9348793475347534",
-//             accessToken: "9e8r9w7r98e7rt98",
-//             apiConnector: {
-//               id: "39749345763945",
-//               name: "Github",
-//               markdown: "This is a description"
-//             },
-//             apiResponses: [
-//               {
-//                 id: "93845793845938",
-//                 response: "{name: recentProjects, data: ['First', 'Second']}",
-//                 apiConnectorRoute: {
-//                   id: "3948579374593",
-//                   name: "RecentProjects",
-//                   url: "/projects/recent"
-//                 }
-//               }
-//             ]
-//           }
-//         ]
-//       }
-//     ]
-//   },
-// ],
--- src/test/seed/index.ts
+++ src/test/seed/index.ts
@@ -5,11 +5,49 @@ import { testData } from "./data";
 export interface SeedData {
   users: {
     email: string,
+    image: string,
+    provider: string,
+    type: string,
+    token_type: string,
+    scope: string,
+    providerAccountId: string,
+  }[],
+  sites: {
+    id: string,
+    subdomain: string,
+    extensions: {
+      id: string,
+      storeExtension: {
+        id: string,
+        name: string,
+      },
+      underlayingApis: {
+        id: string,
+        refreshToken: string,
+        accessToken: string,
+        apiConnector: {
+          id: string,
+          name: string,
+          markdown: string,
+        },
+        apiResponses: {
+          id: string,
+          response: string,
+          apiConnectorRoute: {
+            id: string,
+            name: string,
+            url: string,
+          },
+        }[],
+      }[],
+    }[],
   }[],
 }
 
 // Inspired by prisma/docs#451
 async function emptyDatabase() {
+  console.log("Emptying database");
+  
   const tables = Prisma.dmmf.datamodel.models.map(
     (model) => model.dbName || model.name
   );
@@ -20,6 +58,7 @@ async function emptyDatabase() {
 }
 
 async function seedDatabase({ users }: SeedData) {
+  console.log("Seeding database");
   // Insert users
   await Promise.all(
     users.map((user) =>
@@ -29,25 +68,75 @@ async function seedDatabase({ users }: SeedData) {
     )
   );
 
-//   // Insert projects & connect them to their users
-//   await Promise.all(
-//     projects.map((project) =>
-//       prisma.project.create({
-//         data: {
-//           ...project,
-//           contributors: {
-//             create: [
-//               {
-//                 user: {
-//                   connect: project.users?.map((user) => ({ email: user })),
-//                 },
-//               },
-//             ],
-//           },
-//         },
-//       })
-//     )
-//   );
+  // Insert accounts
+  await Promise.all(
+    users.map((user) =>
+      prisma.account.create({
+        data: {
+          user: {
+            connect: {
+              email: user.email,
+            },
+          },
+          provider: user.provider,
+          type: user.type,
+          token_type: user.token_type,
+          scope: user.scope,
+          providerAccountId: user.providerAccountId,
+        },
+      })
+    )
+  );
+
+  // Insert sites & connect them to their users
+  await Promise.all(
+    testData.sites.map((site) => {
+      return prisma.site.create({
+        data: {
+          id: site.id,
+          subdomain: site.subdomain,
+          user: {
+            connect: {
+              email: testData.users[0].email,
+            },
+          },
+          extensions: {
+            create: site.extensions.map((extension) => ({
+              id: extension.id,
+              storeExtension: {
+                connect: {
+                  id: extension.storeExtension.id,
+                },
+              },
+              underlayingApis: {
+                create: extension.underlayingApis.map((underlayingApi) => ({
+                  id: underlayingApi.id,
+                  refreshToken: underlayingApi.refreshToken,
+                  accessToken: underlayingApi.accessToken,
+                  apiConnector: {
+                    connect: {
+                      id: underlayingApi.apiConnector.id,
+                    },
+                  },
+                  apiResponses: {
+                    create: underlayingApi.apiResponses.map((apiResponse) => ({
+                      id: apiResponse.id,
+                      response: apiResponse.response,
+                      apiConnectorRoute: {
+                        connect: {
+                          id: apiResponse.apiConnectorRoute.id,
+                        },
+                      },
+                    })),
+                  },
+                })),
+              },
+            })),
+          },
+        },
+      });
+    })
+  );
 }
 
 export async function reseedDatabase(data: SeedData = testData) {
