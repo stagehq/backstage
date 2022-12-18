4a2b309fc20f7288247fcee68f4976503c29ce2e
Felix Haeberle <kontakt@felixhaeberle.de>
2022-12-18 23:46:43 +0100

    improve auth

--- src/client/api/connector.ts
+++ src/client/api/connector.ts
@@ -1,6 +1,6 @@
-import { URLSearchParams } from "url";
+import * as crypto from "crypto";
 
-enum RedirectMethod {
+export enum RedirectMethod {
   Web = "web",
   Local = "local",
 }
@@ -12,13 +12,13 @@ interface AuthorizationRequestOptions {
 }
 
 interface AuthorizationRequestURLParams {
-  codeChallenge: string;
+  codeChallenge: Promise<string>;
   codeVerifier: string;
   state: string;
   redirectURI: string;
 }
 
-interface AuthorizationRequest extends AuthorizationRequestURLParams {
+export interface AuthorizationRequest extends AuthorizationRequestURLParams {
   toURL(): string;
 }
 
@@ -40,7 +40,7 @@ interface TokenSet {
   scope?: string;
 }
 
-interface TokenSetOptions {
+export interface TokenSetOptions {
   accessToken: string;
   refreshToken?: string;
   idToken?: string;
@@ -48,7 +48,7 @@ interface TokenSetOptions {
   scope?: string;
 }
 
-interface TokenResponse {
+export interface TokenResponse {
   access_token: string;
   refresh_token?: string;
   id_token?: string;
@@ -62,93 +62,73 @@ interface PKCEClientConfig {
   providerIcon: string;
   providerId?: string;
   description?: string;
-  authorizationRequest: (
-    options: AuthorizationRequestOptions
-  ) => Promise<AuthorizationRequest>;
-  authorize: (
-    options: AuthorizationRequest | AuthorizationOptions
-  ) => Promise<AuthorizationResponse>;
-  setTokens: (options: TokenSetOptions | TokenResponse) => Promise<void>;
-  getTokens: () => Promise<TokenSet | undefined>;
-  removeTokens(): Promise<void>;
+  // authorizationRequest: (
+  //   options: AuthorizationRequestOptions
+  // ) => Promise<AuthorizationRequest>;
+  // authorize: (
+  //   options: AuthorizationRequest | AuthorizationOptions
+  // ) => Promise<AuthorizationResponse>;
+  // setTokens: (options: TokenSetOptions | TokenResponse) => Promise<void>;
+  // getTokens: () => Promise<TokenSet | undefined>;
+  // removeTokens(): Promise<void>;
 }
 
 // interface OAuth {
 //   PKCEClient: (options: PKCEClientConfig) => OAuth["PKCEClient"];
 // }
 
-export const OAuth = {
-  PKCEClient: (options: PKCEClientConfig) => {
-    const {
-      redirectMethod,
-      providerName,
-      providerIcon,
-      providerId,
-      description,
-    } = options;
-    
-    return {
-      redirectMethod,
-      providerName,
-      providerIcon,
-      providerId,
-      description,
-      authorizationRequest: async (options: AuthorizationRequestOptions) => {
-        const { endpoint, clientId, scope, extraParameters } = options;
-        const codeVerifier = generateCodeVerifier();
-        const codeChallenge = generateCodeChallenge(codeVerifier);
-        const state = generateState();
-        const redirectURI = getRedirectURI();
-        const params = new URLSearchParams({
-          response_type: "code",
-          client_id: clientId,
-          scope,
-          code_challenge: await codeChallenge,
-          code_challenge_method: "S256",
-          state,
-          redirect_uri: redirectURI,
-          ...extraParameters,
-        });
-        const url = `${endpoint}?${params.toString()}`;
-        return {
-          codeVerifier,
-          codeChallenge,
-          state,
-          redirectURI,
-          toURL: () => url,
-        };
-      },
-      authorize,
-      setTokens,
-      getTokens,
-      removeTokens,
-    };
-  },
-};
+// export const OAuth = {
+//   PKCEClient: (options: PKCEClientConfig) => {
+//     const {
+//       redirectMethod,
+//       providerName,
+//       providerIcon,
+//       providerId,
+//       description,
+//     } = options;
 
-/* TODO: Implement this function and place them above */
-
-const authorize = async (
-  options: AuthorizationRequest | AuthorizationOptions
-) => {
-  console.log(options);
-};
-
-const setTokens = async (options: TokenSetOptions | TokenResponse) => {
-  console.log(options);
-};
-
-const getTokens = async () => {
-  console.log("getTokens");
-};
-
-const removeTokens = async () => {
-  console.log("removeTokens");
-};
+//     return {
+//       redirectMethod,
+//       providerName,
+//       providerIcon,
+//       providerId,
+//       description,
+//       authorizationRequest: async (options: AuthorizationRequestOptions) => {
+//         const { endpoint, clientId, scope, extraParameters } = options;
+//         const codeVerifier = generateCodeVerifier();
+//         const codeChallenge = generateCodeChallenge(codeVerifier);
+//         const state = generateState();
+//         const redirectURI = getRedirectURI();
+//         const params = new URLSearchParams({
+//           response_type: "code",
+//           client_id: clientId,
+//           scope,
+//           code_challenge: await codeChallenge,
+//           code_challenge_method: "S256",
+//           state,
+//           redirect_uri: redirectURI,
+//           ...extraParameters,
+//         });
+//         const url = `${endpoint}?${params.toString()}`;
+//         return {
+//           codeVerifier,
+//           codeChallenge,
+//           state,
+//           redirectURI,
+//           toURL: () => url,
+//         };
+//       },
+//       authorize,
+//       setTokens,
+//       getTokens,
+//       removeTokens,
+//     };
+//   },
+// };
 
 const generateCodeVerifier = () => {
   const array = new Uint32Array(56);
-  window.crypto.getRandomValues(array);
+  crypto.getRandomValues(array);
   return Array.from(array, (dec) => ("0" + dec.toString(16)).substr(-2)).join(
     ""
   );
@@ -156,7 +136,7 @@ const generateCodeVerifier = () => {
 
 const generateCodeChallenge = (codeVerifier: string) => {
   const buffer = new TextEncoder().encode(codeVerifier);
-  return window.crypto.subtle.digest("SHA-256", buffer).then((hash) => {
+  return crypto.subtle.digest("SHA-256", buffer).then((hash) => {
     const base64 = btoa(String.fromCharCode(...new Uint8Array(hash)));
     return base64.replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
   });
@@ -164,7 +144,7 @@ const generateCodeChallenge = (codeVerifier: string) => {
 
 const generateState = () => {
   const array = new Uint32Array(56);
-  window.crypto.getRandomValues(array);
+  crypto.getRandomValues(array);
   return Array.from(array, (dec) => ("0" + dec.toString(16)).substr(-2)).join(
     ""
   );
@@ -174,3 +154,162 @@ const getRedirectURI = () => {
   const { protocol, host } = window.location;
   return `${protocol}//${host}/api/oauth/callback`;
 };
+
+export class PKCEClient {
+  redirectMethod: RedirectMethod;
+  providerName: string;
+  providerIcon: string;
+  providerId: string | undefined;
+  description: string | undefined;
+
+  constructor(options: PKCEClientConfig) {
+    this.redirectMethod = options.redirectMethod;
+    this.providerName = options.providerName;
+    this.providerIcon = options.providerIcon;
+    this.providerId = options.providerId;
+    this.description = options.description;
+  }
+
+  authorizationRequest = async (
+    options: AuthorizationRequestOptions
+  ): Promise<AuthorizationRequest> => {
+    const { endpoint, clientId, scope, extraParameters } = options;
+
+    console.log(typeof window !== "undefined");
+
+    const generateCodeVerifier = () => {
+      const array = new Uint32Array(56);
+      window.crypto.getRandomValues(array);
+      return Array.from(array, (dec) =>
+        ("0" + dec.toString(16)).substr(-2)
+      ).join("");
+    };
+
+    const generateCodeChallenge = (codeVerifier: string) => {
+      const buffer = new TextEncoder().encode(codeVerifier);
+      return window.crypto.subtle.digest("SHA-256", buffer).then((hash) => {
+        const base64 = btoa(String.fromCharCode(...new Uint8Array(hash)));
+        return base64.replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
+      });
+    };
+
+    const generateState = () => {
+      const array = new Uint32Array(56);
+      window.crypto.getRandomValues(array);
+      return Array.from(array, (dec) =>
+        ("0" + dec.toString(16)).substr(-2)
+      ).join("");
+    };
+
+    const getRedirectURI = () => {
+      const { protocol, host } = window.location;
+      return `${protocol}//${host}/s`;
+    };
+
+    const codeVerifier = generateCodeVerifier();
+    const codeChallenge = generateCodeChallenge(codeVerifier);
+    const state = generateState();
+    const redirectURI = getRedirectURI();
+    const params = new URLSearchParams({
+      response_type: "code",
+      client_id: clientId,
+      scope,
+      code_challenge: await codeChallenge,
+      code_challenge_method: "S256",
+      state,
+      redirect_uri: redirectURI,
+      ...extraParameters,
+    });
+    console.log(params.toString());
+
+    const url = `${endpoint}?${params.toString()}`;
+    return {
+      codeVerifier,
+      codeChallenge,
+      state,
+      redirectURI,
+      ...extraParameters,
+      toURL: () => url,
+    };
+  };
+
+  authorize = async (options: AuthorizationRequest | AuthorizationOptions): Promise<AuthorizationResponse> => {
+    let authorizationRequest: AuthorizationRequest;
+    if ("endpoint" in options) {
+      // options is an AuthorizationRequestOptions object
+      authorizationRequest = this.authorizationRequest(options);
+    } else {
+      // options is an AuthorizationRequest object
+      authorizationRequest = options;
+    }
+
+    // Open the login window
+    const loginWindow = this.popupWindow(
+      authorizationRequest.toURL(),
+      "Login",
+      600,
+      600
+    );
+
+    if (!loginWindow) throw new Error("Could not open login window");
+
+    const response = await this.pollForResponse(loginWindow);
+
+    return response;
+  };
+
+  private popupWindow(url: string, title: string, w: number, h: number) {
+    // Fixes dual-screen position                             Most browsers      Firefox
+    const dualScreenLeft =
+      window.screenLeft !== undefined ? window.screenLeft : window.screenX;
+    const dualScreenTop =
+      window.screenTop !== undefined ? window.screenTop : window.screenY;
+
+    const width = window.innerWidth
+      ? window.innerWidth
+      : document.documentElement.clientWidth
+      ? document.documentElement.clientWidth
+      : screen.width;
+    const height = window.innerHeight
+      ? window.innerHeight
+      : document.documentElement.clientHeight
+      ? document.documentElement.clientHeight
+      : screen.height;
+
+    const left = (width - w) / 2 + dualScreenLeft;
+    const top = (height - h) / 2 + dualScreenTop;
+    const newWindow = window.open(
+      url,
+      title,
+      `
+      scrollbars=yes,
+      width=${w}, 
+      height=${h}, 
+      top=${top}, 
+      left=${left}
+      `
+    );
+
+    return newWindow;
+  }
+
+  private pollForResponse = async (
+    loginWindow: Window
+  ): Promise<{ authorizationCode: string; }> => {
+    // Poll for the response from the login window
+    return new Promise((resolve, reject) => {
+      const interval = setInterval(() => {
+        try {
+          const queryParams = new URLSearchParams(loginWindow.location.search);
+          const authorizationCode = queryParams.get("code");
+          if (authorizationCode) {
+            resolve({ authorizationCode });
+            clearInterval(interval);
+          }
+        } catch (error) {
+          // Ignore the error and continue polling
+        }
+      }, 1000);
+    });
+  };
+}
--- src/client/routes/dashboard/page.tsx
+++ src/client/routes/dashboard/page.tsx
@@ -1,5 +1,5 @@
-import { useEffect } from "react";
 import useSWR from "swr";
+import { AuthorizationRequest, PKCEClient, RedirectMethod, TokenResponse } from "../../api/connector";
 import Banner from "../../components/Banner";
 import Container from "../../components/Container";
 import Content from "../../components/Content";
@@ -19,14 +19,79 @@ const useLinkedin = (url: string) => {
   };
 };
 
-const Dashboard = () => {
-  const { user, isLoading, isError } = useLinkedin(
-    "https://linkedin.com/in/brunocampos01"
-  );
+export async function fetchTokens(
+  authRequest: AuthorizationRequest,
+  authCode: string
+): Promise<TokenResponse> {
+  const response = await fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" , "Access-Control-Allow-Origin": "*"},
+    body: JSON.stringify({
+      client_id: "d088936f563ce38a44a1",
+      client_secret: "xT3vOix9IMKbEmHiUt-kSScEdrlG7GT51B9IH4uAJbwEbZ4aA7",
+      code: authCode,
+      // code_verifier: authRequest.codeVerifier,
+      // grant_type: "authorization_code",
+      redirect_uri: authRequest.redirectURI,
+    }),
+  });
+  if (!response.ok) {
+    console.error("fetch tokens error:", await response.text());
+    throw new Error(response.statusText);
+  }
+  return (await response.json()) as TokenResponse;
+}
 
-  useEffect(() => {
-    console.log(user);
-  }, [user]);
+const Dashboard = () => {
+  // const { user, isLoading, isError } = useLinkedin(
+  //   "https://linkedin.com/in/brunocampos01"
+  // );
+
+  const handleClick = async () => {
+    async function fetchTokens(
+      authRequest: AuthorizationRequest,
+      authCode: string
+    ): Promise<TokenResponse> {
+      const response = await fetch("https://github.com/login/oauth/access_token", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" , "Access-Control-Allow-Origin": "*"},
+        body: JSON.stringify({
+          client_id: "d088936f563ce38a44a1",
+          client_secret: "xT3vOix9IMKbEmHiUt-kSScEdrlG7GT51B9IH4uAJbwEbZ4aA7",
+          code: authCode,
+          code_verifier: authRequest.codeVerifier,
+          grant_type: "authorization_code",
+          redirect_uri: authRequest.redirectURI,
+        }),
+      });
+      if (!response.ok) {
+        console.error("fetch tokens error:", await response.text());
+        throw new Error(response.statusText);
+      }
+      return (await response.json()) as TokenResponse;
+    }
+
+    const client = new PKCEClient({
+      redirectMethod: RedirectMethod.Local,
+      providerName: "GitHub",
+      providerIcon: "https://github.githubassets.com/favicon.ico",
+      providerId: "gitHub",
+      description: "GitHub OAuth",
+    });
+
+    const authRequest = await client
+      .authorizationRequest({
+        endpoint: "https://github.com/login/oauth/authorize",
+        clientId: "d088936f563ce38a44a1",
+        scope: "read:user,repo",
+      });
+
+    const { authorizationCode } = await client.authorize(authRequest);
+    console.log(authorizationCode);
+
+    const response = await fetchTokens(authRequest, authorizationCode);
+    console.log(response);
+  };
 
   return (
     <>
@@ -35,6 +100,7 @@ const Dashboard = () => {
           <Banner />
           <Title />
           <More />
+          <span onClick={handleClick}>Connect GitHub</span>
         </Container>
       </Content>
     </>
