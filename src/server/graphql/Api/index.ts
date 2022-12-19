ba2548406cd49d976725b308bea35a2b0247168a
Nils Jacobsen <nils.jacobsen98@gmail.com>
2022-12-19 13:06:15 +0100

    added createApi mutation

--- src/server/graphql/Api/index.ts
+++ src/server/graphql/Api/index.ts
@@ -1,4 +1,5 @@
 import { builder } from '../builder';
+import prisma from "../../db/prisma";
 
 builder.prismaNode('Api', {
   findUnique: (id) => ({ id: id }),
@@ -6,10 +7,43 @@ builder.prismaNode('Api', {
   fields: (t) => ({
     createdAt: t.string({ resolve: site => site.createdAt.toString()}),
     modifiedAt: t.string({ resolve: site => site.modifiedAt.toString()}),
-    refreshToken: t.exposeString('refreshToken'),
     accessToken: t.exposeString('accessToken'),
+    isExpired: t.exposeBoolean('isExpired'),
 
     apiResponses: t.relation('apiResponses'),
     apiConnector: t.relation('apiConnector'),
+
+    extension: t.relation('extension')
   }),
 });
+
+builder.mutationField('createApi', (t) => 
+  t.prismaField({
+    type: "Api",
+    args: {
+      extensionId: t.arg.string(),
+      apiConnectorId: t.arg.string(),
+      accessToken: t.arg.string()
+    },
+    resolve: async (query, root, args, ctx) => {
+      if (!ctx.session.user.email || args.extensionId === undefined || args.extensionId === null || args.apiConnectorId === null || args.accessToken === null || args.accessToken === undefined) return null;
+
+      const api = await prisma.api.create({
+        data: {
+          apiConnector: {
+            connect: {
+              id: args.apiConnectorId
+            }
+          },
+          extension: {
+            connect: {
+              id: args.extensionId
+            }
+          },
+          accessToken: args.accessToken
+        }
+      })
+      return api;
+    }
+  })
+);
