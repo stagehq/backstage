3d42acedc4d26c4ef3d225152b8fc4b2a0177d9b
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-01-12 23:35:34 +0100

    fix: show text on reload

--- src/client/components/PageHeader.tsx
+++ src/client/components/PageHeader.tsx
@@ -1,9 +1,11 @@
 import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { toast } from "react-hot-toast";
 import { useRecoilState, useRecoilValue } from "recoil";
+import update from "../../pages/api/nonOAuth/devto/update";
 import { useUpdateSiteHeaderMutation } from "../graphql/updateSiteHeader.generated";
 import { siteSlugState, siteState } from "../store/site";
+import TextareaAutosize from 'react-textarea-autosize';
 
 export interface PageHeaderProps {
   title: string;
@@ -20,13 +22,10 @@ export const PageHeader = ({
   lightMode,
   toggleLightMode,
 }: PageHeaderProps) => {
-  //refs
-  const bioRef = useRef<HTMLTextAreaElement>(null);
-  const tagLineRef = useRef<HTMLTextAreaElement>(null);
 
   //local state
-  const [bio, setBio] = useState<string>("");
-  const [tagLine, setTagLine] = useState<string>("");
+  const [bio, setBio] = useState<string>(description); 
+  const [tagLine, setTagLine] = useState<string>(title);
 
   //fetches
   const [, updateSiteHeader] = useUpdateSiteHeaderMutation();
@@ -34,31 +33,6 @@ export const PageHeader = ({
   //state
   const siteSlug = useRecoilValue(siteSlugState);
   const [site] = useRecoilState(siteState(siteSlug));
-
-  useEffect(() => {
-    if (bioRef.current?.style) {
-      bioRef.current.style.height = "0px";
-      const scrollHeight = bioRef.current.scrollHeight;
-      bioRef.current.style.height = scrollHeight + "px";
-    }
-  }, [bio]);
-
-  useEffect(() => {
-    if (tagLineRef.current?.style) {
-      console.log(tagLineRef.current.scrollHeight);
-      tagLineRef.current.style.height = "0px";
-      const scrollHeight = tagLineRef.current.scrollHeight;
-      tagLineRef.current.style.height = scrollHeight + "px";
-    }
-  }, [tagLine]);
-
-  useEffect(() => {
-    if(document.readyState === "complete"){
-      setBio(description);
-      setTagLine(title);
-    }
-
-  }, [description, title, tagLineRef, bioRef]);
 
   const handleBlur = async () => {
     if (bio !== description || tagLine != description) {
@@ -98,25 +72,21 @@ export const PageHeader = ({
       <div className="flex flex-col justify-start items-start self-stretch gap-8 w-full @3xl:w-3/4 @6xl:w-1/2">
         <img src={image} className="w-16 rounded-full object-cover" />
         <div className="w-full flex flex-col gap-4">
-          <textarea
-            ref={tagLineRef}
-            name="title"
-            id="title"
-            className="block w-full resize-none bg-white focus:bg-white dark:bg-zinc-900 py-0 px-0 font-bold text-2xl lg:text-4xl text-zinc-800 dark:text-zinc-200 placeholder-zinc-300 focus:ring-transparent border-0 border-l-2 pl-4 -ml-4 border-transparent hover:border-zinc-300 active:border-zinc-600 focus:border-zinc-600"
-            placeholder="Enter tagline..."
-            value={tagLine}
+          <TextareaAutosize 
+            value={tagLine} 
             onChange={(e) => setTagLine(e.target.value)}
             onBlur={() => handleBlur()}
+            id="tagline"
+            placeholder="Enter tagline..."
+            className="block w-full resize-none bg-white focus:bg-white dark:bg-zinc-900 py-0 px-0 font-bold text-2xl lg:text-4xl text-zinc-800 dark:text-zinc-200 placeholder-zinc-300 focus:ring-transparent border-0 border-l-2 pl-4 -ml-4 border-transparent hover:border-zinc-300 active:border-zinc-600 focus:border-zinc-600"
           />
-          <textarea
-            ref={bioRef}
-            name="title"
-            id="title"
-            className="block w-full resize-none bg-white focus:bg-white dark:bg-zinc-900 py-0 px-0 text-sm text-zinc-800 dark:text-zinc-200 placeholder-zinc-300 focus:ring-transparent border-0 border-l-2 pl-4 -ml-4 border-transparent hover:border-zinc-300 active:border-zinc-600 focus:border-zinc-600"
-            placeholder="Enter bio..."
-            value={bio}
+          <TextareaAutosize 
+            value={bio} 
             onChange={(e) => setBio(e.target.value)}
             onBlur={() => handleBlur()}
+            id="bio"
+            placeholder="Enter bio..."
+            className="h-0 min-h-0 overflow-hidden block w-full resize-none bg-white focus:bg-white dark:bg-zinc-900 py-0 px-0 text-sm text-zinc-800 dark:text-zinc-200 placeholder-zinc-300 focus:ring-transparent border-0 border-l-2 pl-4 -ml-4 border-transparent hover:border-zinc-300 active:border-zinc-600 focus:border-zinc-600"
           />
         </div>
       </div>
