9500c0597c657b4326414885d48186d7989ac46b
Nils Jacobsen <nils.jacobsen98@gmail.com>
2023-02-09 12:13:19 +0100

    fix: isr rendering

--- src/client/components/isr/SitePage.tsx
+++ src/client/components/isr/SitePage.tsx
@@ -109,7 +109,7 @@ const SitePage: FC = () => {
                   cols={{ lg: 3, md: 2, sm: 1 }}
                   rowHeight={1}
                   width={1000}
-                  margin={[32, 0]}
+                  margin={[32, 32]}
                   isResizable={false}
                   isDraggable={false}
                   measureBeforeMount={true}
