7c88fbe1ede9136180e1825e9a34521972d86c4a
Felix Haeberle <kontakt@felixhaeberle.de>
2023-02-15 15:18:34 +0100

    make seeding deploy save

--- src/pages/api/seed/storeExtensions.ts
+++ src/pages/api/seed/storeExtensions.ts
@@ -6,6 +6,10 @@ import {
 } from "../../../test/seed/data";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await reseedDatabase(apiConnectorsData, storeExtensionsData);
-  return res.status(500).json({ result: "Data seeded" });
+  if (process.env.NODE_ENV !== "development") {
+    return res.status(500).json({ result: "Not in dev mode" });
+  } else {
+    await reseedDatabase(apiConnectorsData, storeExtensionsData);
+    return res.status(500).json({ result: "Data seeded" });
+  }
 };
