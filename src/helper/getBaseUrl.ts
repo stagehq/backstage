9b5dd222107454d2489806b4f9aac224042653bd
Felix Haeberle <kontakt@felixhaeberle.de>
2023-02-24 11:09:57 +0100

    update baseurl to match netlify env #37

--- src/helper/getBaseUrl.ts
+++ src/helper/getBaseUrl.ts
@@ -3,5 +3,5 @@ export const getBaseUrl = () => {
     return `${process.env.NEXT_PUBLIC_HOST_URL}`;
   }
 
-  return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  return `${process.env.URL}`;
 };
