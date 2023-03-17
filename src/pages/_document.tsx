88359e552019c42f5f0b998de2f37170a08c9e25
NiklasBuchfink <niklas.buchfink@web.de>
2023-03-17 11:36:06 +0100

    edit manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,7 +32,7 @@ class MyDocument extends Document {
             sizes="16x16"
             href="/favicons/favicon-16x16.png"
           />
-          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link rel="manifest" href="/site.webmanifest" />
           <link
             rel="mask-icon"
             href="/favicons/safari-pinned-tab.svg"
