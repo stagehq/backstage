83a86938e887d5674467bb12d729234f41615db9
Felix Haeberle <kontakt@felixhaeberle.de>
2023-03-23 14:51:33 +0100

    update fallback routes when not logged in #168

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -6,8 +6,21 @@ import Modals from "../../client/components/Modals";
 import Dashboard from "../../client/routes/dashboard/page";
 import Site from "../../client/routes/site";
 import SiteSettings from "../../client/routes/site/siteSettings";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
 
 export default function App() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") return <LoadingPage />;
+
+  if (status === "unauthenticated") {
+    router.push("/auth/login");
+    return <LoadingPage />;
+  }
+
   return (
     <>
       <Router>
